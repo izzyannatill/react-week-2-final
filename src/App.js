5af00a6e1364c2cca824f0d12b993427f31524b2
izzyannatill <izzyannatill@gmail.com>
2025-03-04 17:14:25 +0000

    Removed unneeded files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import './style.css';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
