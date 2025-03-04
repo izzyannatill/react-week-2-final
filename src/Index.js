ee481a26245a2f4afdebb6bd59f3010ddb76d71c
izzyannatill <izzyannatill@gmail.com>
2025-03-04 17:29:32 +0000

    Updated code

--- src/Index.js
+++ src/Index.js
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import './style.css';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
