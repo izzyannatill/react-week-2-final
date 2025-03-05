eb24d3031904851ae0560fdde2ce10aa7ea070a8
izzyannatill <izzyannatill@gmail.com>
2025-03-05 18:02:44 +0000

    Update Index.js

--- src/Index.js
+++ src/Index.js
@@ -2,11 +2,11 @@ import React from "react";
 import Weather from "./Weather.js";
 import '.style.css';
 
-function App() {
+export default function App() {
     return (
-    <div className="App">
-      <h1>Weather App</h1>
-      <Weather />
-</div>
-    );
-    }
+        <div className="App">
+            <h1>Weather</h1>
+            <Weather />
+        </div>
+    )
+}
