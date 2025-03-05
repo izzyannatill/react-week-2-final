8461bd397ffd16dc37501e3514b5f9a1d341a683
izzyannatill <izzyannatill@gmail.com>
2025-03-05 17:49:49 +0000

    Update Index.js

--- src/Index.js
+++ src/Index.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { StrictMode } from 'react';
-import Weather from "./Weather";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
+
+import Weather from "./Weather.js";
 
 root.render(
   <StrictMode>
