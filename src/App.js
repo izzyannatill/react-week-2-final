fef33659bd443617d0ca94fe5fcb7189c481954e
izzyannatill <izzyannatill@gmail.com>
2025-03-06 18:27:41 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import "./styles.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
+      <h1>Weather Search Engine!</h1>
       <WeatherSearch />
     </div>
   );
