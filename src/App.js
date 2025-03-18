6ff26fdbc7970d2a3ae594626f076ad8b4e2f7e8
izzyannatill <izzyannatill@gmail.com>
2025-03-18 18:47:12 +0000

    Added link

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ export default function App() {
     <div className="App">
       <h1>Weather Search Engine!</h1>
       <WeatherSearch />
+      <p>Created by Izzy on <a href="https://github.com/izzyannatill/react-week-2-final">Github</a></p>
     </div>
   );
 }
